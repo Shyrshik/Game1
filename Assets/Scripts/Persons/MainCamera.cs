c0494d2c8092e7beaffb6351651e0d85f5f4a664
Shyrshik <mr.gitarast@mail.ru>
2023-03-02 13:37:40 +0600

    1

--- Assets/Scripts/Persons/MainCamera.cs
+++ Assets/Scripts/Persons/MainCamera.cs
@@ -6,15 +6,16 @@ public class MainCamera : MonoBehaviour
 {
     [SerializeField] private GameObject _trackingObject;
     [SerializeField] private Vector3 _positionCorrect = new(0f, -10f, -6);
-    [SerializeField,Tooltip("Отрицательное значение = Установить автоматически.")] private float _maxDistanceToTrackingObject = -1;
+    [SerializeField, Tooltip("Отрицательное значение = Установить автоматически.")] private float _maxDistanceToTrackingObject = -1;
     private Rigidbody2D _rigidBody2D;
     private Moved _trackingObjectMove;
     private Rigidbody2D _trackingObjectRigidBody2D;
     private Vector2 _moveCamera;
     private Vector2 _correct;
+    private GameObject _noTarget = null;
     private void Awake()
     {
-        SetTrackingObject();
+        SetTrackingObject(_trackingObject);
         _correct = _positionCorrect;
         _rigidBody2D = GetComponent<Rigidbody2D>();
         if (_maxDistanceToTrackingObject < 0f)
@@ -23,7 +24,7 @@ public class MainCamera : MonoBehaviour
     private void FixedUpdate()
     {
         _moveCamera = _trackingObjectRigidBody2D.position + _correct - _rigidBody2D.position;
-        _moveCamera += Vector2.ClampMagnitude(_trackingObjectMove.VectorVelocity, _maxDistanceToTrackingObject) 
+        _moveCamera += Vector2.ClampMagnitude(_trackingObjectMove.VectorVelocity, _maxDistanceToTrackingObject)
             + _trackingObjectRigidBody2D.velocity;
         _rigidBody2D.velocity = _moveCamera;
     }
@@ -31,14 +32,25 @@ public class MainCamera : MonoBehaviour
     {
         _maxDistanceToTrackingObject = GetComponent<Camera>().orthographicSize * 1.7f;
     }
-    public void SetTrackingObject(GameObject trackingObject = null)
+    public void SetTrackingObject(GameObject trackingObject)
     {
-        _trackingObject = trackingObject ? trackingObject : _trackingObject;
-        _trackingObject = _trackingObject ? _trackingObject : GameObject.FindGameObjectWithTag("Player");
-        _trackingObject = _trackingObject ? _trackingObject : gameObject;
-        _trackingObjectMove = _trackingObject.GetComponent<Moved>();
-        _trackingObjectRigidBody2D = _trackingObject.GetComponent<Rigidbody2D>();
-        transform.position = _positionCorrect + _trackingObject.transform.position;
+        DestroyImmediate(_noTarget);
+        try
+        {
+            _trackingObject = trackingObject;
+            _trackingObjectMove = _trackingObject.GetComponent<Moved>();
+            _trackingObjectRigidBody2D = _trackingObject.GetComponent<Rigidbody2D>();
+            transform.position = _positionCorrect + _trackingObject.transform.position;
+        }
+        catch 
+        {
+            Debug.LogError("NoTargetForCamera");
+            _trackingObject = new("NoTargetForCamera");
+            _trackingObject.transform.position = Vector3.zero;
+            _trackingObject.AddComponent<Moved>();
+            _trackingObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            SetTrackingObject(_trackingObject);
+        }
 
 
     }
