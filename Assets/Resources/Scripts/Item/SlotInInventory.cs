75ab65b30a0652db047a39ef9399fc25ec3385f5
Shyrshik <mr.gitarast@mail.ru>
2023-03-12 11:46:01 +0600

    SlotInInventory public static readonly SlotInWorld Empty;

--- Assets/Resources/Scripts/Item/SlotInInventory.cs
+++ Assets/Resources/Scripts/Item/SlotInInventory.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 public class SlotInInventory : MonoBehaviour
 {
     public Item Item { get => _item; }
+    public static readonly SlotInWorld Empty;
     protected Item _item = Item.Empty;
 
     private static Sprite _defaultSprite;
     private Image _image;
+    static SlotInInventory()
+    {
+        if (Empty == null)
+        {
+            Empty = new GameObject().AddComponent<SlotInWorld>();
+        }
+    }
     public bool AddItem(Item item)
     {
         if (IsEmpty())
