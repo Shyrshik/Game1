fd6592efd81f61f40178259f977c84a3d3bbd006
Shyrshik <mr.gitarast@mail.ru>
2023-11-09 23:37:16 +0600

    сделал другой алгоритм генерации мира.

--- Assets/Resources/Scripts/Terrain/MapBuilder.cs
+++ Assets/Resources/Scripts/Terrain/MapBuilder.cs
@@ -29,9 +29,54 @@ public class MapBuilder : MonoBehaviour
     {
         _tilesLeft = _countTiles;
         _sizeSelfMap = _sizeMap / 2;
-        MazeWalkers();
+        RandomPoint();
         SetWallAroundGround();
     }
+    private void RandomPoint()
+    {
+        //caching
+        Vector3Int startPosition = new(0, 0, 0);
+        Vector3Int positionNew;
+        int walkerNumber = 0;
+        List< Vector3Int> emptyPositions = new(_countTiles*2);
+
+        //Set start platform.
+        FillSquareAroundThePoint(startPosition, 1);
+        emptyPositions.Add(new(-1, -2, 0));
+        emptyPositions.Add(new(0, -2, 0));
+        emptyPositions.Add(new(1, -2, 0));
+        emptyPositions.Add(new(-1, 2, 0));
+        emptyPositions.Add(new(0, 2, 0));
+        emptyPositions.Add(new(1, 2, 0));
+        emptyPositions.Add(new(-2, -1, 0));
+        emptyPositions.Add(new(-2, 0, 0));
+        emptyPositions.Add(new(-2, 1, 0));
+        emptyPositions.Add(new(2, -1, 0));
+        emptyPositions.Add(new(2, 0, 0));
+        emptyPositions.Add(new(2, 1, 0));
+
+        // Generate
+        while (_tilesLeft > 0)
+        {
+            _random = UnityEngine.Random.Range(0, _emptyPositions.Count);
+            positionNew = emptyPositions[_random];
+            _tilemapGround.SetTile(positionNew, _tileGround);
+            emptyPositions.RemoveAt(_random);
+
+            for (_i = 0; _i < _tablePositions.Length; _i++)
+            {
+                _position = _tablePositions[_i] + positionNew;
+                if (_tilemapGround.GetTile(_position) is null &&
+                    !emptyPositions.Contains(_position))
+                {
+                    emptyPositions.Add(_position);
+                }
+            }
+            _tilesLeft--;
+        }
+    }
+
+    #region Walkers
     private void MazeWalkers()
     {
         //caching
@@ -160,8 +205,8 @@ public class MapBuilder : MonoBehaviour
             }
         }
     }
-
-
+    #endregion
+    #region Legasi
     private void SimpleRandom()
     {
 
@@ -246,4 +291,5 @@ public class MapBuilder : MonoBehaviour
             }
         }
     }
+    #endregion
 }
