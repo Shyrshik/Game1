9550fb9bb047934f956e6318230877ad8dc2b7b5
Shyrshik <mr.gitarast@mail.ru>
2023-11-21 04:07:17 +0600

    начал переделывать поиск терейна

--- Assets/Resources/Scripts/Terrain/RandomPointAround.cs
+++ Assets/Resources/Scripts/Terrain/RandomPointAround.cs
@@ -7,17 +7,17 @@ namespace Terrain
 {
     public class RandomPointAround : MapGenerator
     {
-        private List<Vector2Int> FindAllInRectUnsafe(Vector2Int begin, Vector2Int end, List<PointType> pointTypes = null)
+        private List<Vector2Int> FindAllInRectUnsafe(Vector2Int begin, Vector2Int end, IEnumerable<PointType> pointTypes = null)
         {
             return FindAllInRectUnsafe(begin.x, begin.y, end.x, end.y, pointTypes);
 
         }
-        private List<Vector2Int> FindAllInRectUnsafe(int x1, int y1, int x2, int y2, List<PointType> pointTypes = null)
+        private List<Vector2Int> FindAllInRectUnsafe(int x1, int y1, int x2, int y2, IEnumerable<PointType> pointTypes = null)
         {
             List<Vector2Int> result = new List<Vector2Int>(9);
             CorrectMinMax(ref x1, ref x2);
             CorrectMinMax(ref y1, ref y2);
-            bool pointTypesIsEmpty = pointTypes is null || pointTypes.Count == 0;
+            bool pointTypesIsEmpty = pointTypes is null || pointTypes.Count() == 0;
             for (i = x1; i <= x2; i++)
             {
                 for (j = y1; j <= y2; j++)
@@ -31,7 +31,7 @@ namespace Terrain
             }
             return result;
         }
-        private IOrderedEnumerable<Vector2Int> FindAllInRadius(Vector2Int centralPosition, int radius, List<PointType> pointTypes = null)
+        private IOrderedEnumerable<Vector2Int> FindAllInRadius(Vector2Int centralPosition, int radius, IEnumerable<PointType> pointTypes = null)
         {
             if (!IsPointInMap(centralPosition))
             {
@@ -49,7 +49,7 @@ namespace Terrain
                 .Where(v => (v - centralPosition).sqrMagnitude <= radius)
                 .OrderBy(v => (v - centralPosition).sqrMagnitude);
         }
-        private IOrderedEnumerable<Vector2Int> FindNearest(Vector2Int centralPosition, List<PointType> pointTypes = null)
+        private IOrderedEnumerable<Vector2Int> FindNearest(Vector2Int centralPosition, IEnumerable<PointType> pointTypes = null)
         {
             IOrderedEnumerable<Vector2Int>  result;
             j = MapMaxX > MapMaxY ? MapMaxX : MapMaxY;
@@ -70,20 +70,21 @@ namespace Terrain
         private List<Vector2Int> FindTerrain(Vector2Int startPoint)
         {
             List<Vector2Int> result = new List<Vector2Int>(1) { startPoint};
-            List<Vector2Int> newPoints = new List<Vector2Int>(1) {startPoint};
-            List<PointType> terrainType = new List<PointType>(1){ _map[startPoint.x,startPoint.y] };
+            List<Vector2Int> newPoints = new List<Vector2Int>(result);
+            List<Vector2Int> oldPoints;
+            PointType[] terrainType = { _map[startPoint.x,startPoint.y] };
             Vector2Int point;
             do
             {
-                point = newPoints[0];
-                newPoints = newPoints.Union(FindAllInRadius(point, 1, terrainType).Except(result)).ToList();
-                result = result.Union(newPoints).ToList();
-                newPoints.RemoveAt(0);
+                //point = newPoints[0];
+                //newPoints = newPoints.Union(FindAllInRadius(point, 1, terrainType).Except(result)).ToList();
+                //result = result.Union(newPoints).ToList();
+                //newPoints.RemoveAt(0);
             }
             while (newPoints.Count() > 0);
             return result;
         }
-        private IEnumerable<Vector2Int> FindAllAroundTerrain(Vector2Int startPoint, List<PointType> pointTypes = null)
+        private IEnumerable<Vector2Int> FindAllAroundTerrain(Vector2Int startPoint, IEnumerable<PointType> pointTypes = null)
         {
             IEnumerable<Vector2Int> result = new Vector2Int[0];
             foreach (Vector2Int point in FindTerrain(startPoint))
@@ -103,7 +104,7 @@ namespace Terrain
             _map = new PointType[MapMaxX, MapMaxY];
             Vector2Int positionNew;
             List< Vector2Int> emptyPositions = new(countPoints*2);
-            List<PointType> PointTypeEmpty = new List<PointType>(1) { PointType.Empty };
+            PointType[] PointTypeEmpty = { PointType.Empty };
 
             //Set start platform.
             FillSquareAroundThePoint(startPosition, 1, PointType.AnyGround, true);
